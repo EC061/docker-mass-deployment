55b45b80b39d86cfd78075887aa755f1c7afafa9
Edward <51186528+EC061@users.noreply.github.com>
2025-03-24 02:03:52 -0400

    updated base image and build script

--- core/docker/entrypoint.sh
+++ core/docker/entrypoint.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 set -e
-
-USERNAME=${USERNAME:-defaultuser}
+# echo "Initializing container for group: $GROUP"
 echo "Initializing container for user: $USERNAME"
-# Check and create user if needed
+
+# Add user and confifure password
 if ! id "$USERNAME" &>/dev/null; then
-    echo "User $USERNAME does not exist, creating..."
     useradd -m -s /bin/bash "$USERNAME"
     adduser "$USERNAME" sudo
     echo "User $USERNAME created and added to sudo group"
 fi
-
 if [ -n "$PASSWORD" ]; then
     echo "Setting password for $USERNAME..."
     echo "$USERNAME:$PASSWORD" | chpasswd
 fi
+
+# Create sample project with pytorch
+mkdir -p "/home/$USERNAME/sample_project_torch"
+cd "/home/$USERNAME/sample_project_torch"
+echo -e "torch==2.6.0\ntorchvision==0.21.0\ntorchaudio==2.6.0\npandas\nmatplotlib\nipython\nipykernel" >> "requirements.txt"
+
+# Create sample project with tensorflow
+mkdir -p "/home/$USERNAME/sample_project_tf"
+cd "/home/$USERNAME/sample_project_tf"
+echo -e "tensorflow[and-cuda]==2.19.0\npandas\nmatplotlib\nipython\nipykernel" >> "requirements.txt"
 
 echo "Initialization complete, starting service..."
 exec "$@"
